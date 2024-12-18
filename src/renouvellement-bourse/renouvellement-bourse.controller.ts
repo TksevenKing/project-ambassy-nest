64b5c3141660ac29f27d83e968bfcdbde7913452
Sanogo0710 <sanogobouaali9@gmail.com>
2024-12-18 19:50:35 +0100

    first part of Renouvellement

--- src/renouvellement-bourse/renouvellement-bourse.controller.ts
+++ src/renouvellement-bourse/renouvellement-bourse.controller.ts
@@ -1,19 +1,47 @@
-import { Controller, Get, Logger, Param } from '@nestjs/common';
+import { Controller, Get, Logger, Param, Post, Put, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express/multer';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { RenouvellementBourseService } from './renouvellement-bourse.service';
+import * as fs from 'fs';
 
 @Controller('renouvellement')
 export class RenouvellementBourseController {
-    @Get()
-    getAllEtudiant() {
-        Logger.log('recupere tous les etudiants', 'EtudiantController');
-        return 'retourne les etudiants';
+    constructor(private readonly renouvellementservice: RenouvellementBourseService){}
+    
+    @Get(':nom')
+    getRenouvellement( nom: string){
+        return "x"
     }
-    @Get(':etudiant_id')
-    getOneEtudiant(@Param('etudiant_id') etudiant_id) {
-        Logger.log('recupere un seul etudiant', 'EtudiantController');
-        return 'recupere un seul etudiant';
+    
+    @Post()
+    @UseInterceptors(FilesInterceptor('files',2,{
+        storage: diskStorage({
+          destination: (req, file, callback) =>{
+            
+            const dest = `./Fichiers/${new Date().toISOString().split('T')[0]}`
+            fs.mkdirSync(dest, { recursive: true });
+            callback(null, dest)
+          },
+          filename: (req, file, callback) => {
+            const uniqueSuffix = Date.now()
+            const ext = extname(file.originalname)
+            const filename = `${file.originalname}-${uniqueSuffix}${ext}`
+            callback(null, filename);
+          },
+        }),
+      }))
+    handleUpload(@UploadedFile() file: Express.Multer.File,@Param('nom') nom: String){
+        return "Upload success"
     }
-    @Get('status/:email')
-    getStatusEtudiant(@Param('email') email) {
-        Logger.log('retourne si un etudiant est boursier ou pas', 'EtudiantController');
+
+    @Put(':renouvelement_id')
+    updateFile(){
+        Logger.log('Modification du dossier.zip étudiant', 'EtudiantController');
+    }
+
+    @Delete(':renouvellement_id')
+    deleteFile(){
+        Logger.log('Supprimer un dossier.zip etudiant', 'EtudiantController');
     }
 }
--- src/renouvellement-bourse/renouvellement-bourse.service.ts
+++ src/renouvellement-bourse/renouvellement-bourse.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RenouvellementBourseEntity } from 'src/entities/renouvellementBourse.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class RenouvellementBourseService {}
+export class RenouvellementBourseService {
+    constructor(@InjectRepository(RenouvellementBourseEntity)  private readonly renouvellementRepository : Repository <RenouvellementBourseEntity>){}
+
+    getAllEtudiant(){
+        return this.renouvellementRepository.find()
+    }
+
+}
